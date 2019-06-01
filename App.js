8431568ef7c8b838c703f938d29cd3ff8f075516
Akshay <akki9270@gmail.com>
2019-06-01 18:11:27 +0530

    Header page, Vinsaelir page & Frettir page

--- App.js
+++ App.js
@@ -7,43 +7,35 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    createStackNavigator,
+    createAppContainer
+} from 'react-navigation';
+import Header from './screens/Header';
+import Vinsaelir from './screens/Vinsaelir';
+import Frettir from './screens/Frettir';
+import Frett from './screens/Frett';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+
+const RootStack = createStackNavigator({
+    Frettir: {
+        screen : Frettir,
+    },
+   /* Vilsaelir: {
+        screen : Vinsaelir,
+    },
+    Head: {
+        screen: Header,
+    },
+   Frett: {
+       screen : Frett,
+   },*/
+},{
+    defaultNavigationOptions : {
+        header : null
+    }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const App = createAppContainer(RootStack);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
